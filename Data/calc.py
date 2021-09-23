75704712e79ffb1f93d2feb22c8d7e44f5f88ab0
Andreas Evensen <andreas.evensen11@gmail.com>
2021-09-23 11:37:51 +0200

    test

---
+++ Data/calc.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+gamma_h = 1
+gamma_n = 1
+k_b = 1
+
+
+
+
+def population(T : float):
+	"""Temperature float, referring to hot/cold-reseveoir """
+	n = 1/(np.exp(k_bT)-1):
+	return n
+
